ccb5b0de64abc0c1010dabe2ffbb499644edffc0
killbasa <killbasab@gmail.com>
2023-10-28 17:17:56 -0400

    fix: dont fetch banner

--- src/routes/(redirects)/invite/+page.server.ts
+++ src/routes/(redirects)/invite/+page.server.ts
@@ -3,26 +3,11 @@ import type { PageServerLoad } from './$types';
 import { isBotUserAgent } from '$lib/utils/constants';
 import { env } from '$env/dynamic/private';
 
-export const load: PageServerLoad = async ({ request, fetch }) => {
+export const load: PageServerLoad = async ({ request }) => {
 	const { headers } = request;
 
 	const userAgent = headers.get('User-Agent') ?? headers.get('user-agent');
 	if (!userAgent || !isBotUserAgent(userAgent)) {
 		throw redirect(301, env.DISCORD_INVITE);
 	}
-
-	const response = await fetch(env.DISCORD_INVITE, {
-		method: 'GET'
-	});
-
-	const html = await response.text();
-	const match = html.match(
-		/https:\/\/cdn\.discordapp\.com\/splashes\/864993757925867530\/(\w+).jpg/
-	);
-	const spashId = match?.at(1);
-	if (!spashId) return;
-
-	return {
-		banner: `https://cdn.discordapp.com/splashes/864993757925867530/${spashId}.png?size=512`
-	};
 };
--- src/routes/(redirects)/invite/+page.svelte
+++ src/routes/(redirects)/invite/+page.svelte
@@ -1,9 +1,3 @@
-<script lang="ts">
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-</script>
-
 <svelte:head>
 	<meta property="og:url" content="https://petracord.com/" />
 	<meta property="og:type" content="website" />
@@ -11,8 +5,4 @@
 	<meta property="og:description" content="Join the Petra Gurin fan Discord!" />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="theme-color" content="#FFAE42" />
-
-	{#if data.banner}
-		<meta property="og:image" content={data.banner} />
-	{/if}
 </svelte:head>
