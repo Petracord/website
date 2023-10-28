7a4dc6aca15ecf7065a635d8caa4322cc837e025
killbasa <killbasab@gmail.com>
2023-10-28 14:50:26 -0400

    feat: organization

--- src/routes/(redirects)/invite/+page.server.ts
+++ src/routes/(redirects)/invite/+page.server.ts
@@ -1,9 +1,12 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { DISCORD_UA } from '$lib/utils/constants';
 
 export const load: PageServerLoad = ({ request }) => {
-	const userAgent = request.headers.get('user-agent');
-	if (!userAgent?.includes('Discordbot')) {
+	const { headers } = request;
+
+	const userAgent = headers.get('User-Agent') ?? headers.get('user-agent');
+	if (userAgent !== DISCORD_UA) {
 		throw redirect(301, 'https://discord.gg/TcXBVgepFD');
 	}
 };
--- src/routes/(redirects)/invite/+page.svelte
+++ src/routes/(redirects)/invite/+page.svelte
@@ -2,6 +2,6 @@
 	<meta property="og:url" content="https://petracord.com/" />
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content="Petracord Invite" />
-	<meta property="og:description" content="A website for the Petra Gurin fan discord." />
+	<meta property="og:description" content="Join the Petra Gurin fan Discord!" />
 	<meta name="theme-color" data-react-helmet="true" content="#FFAE42" />
 </svelte:head>
