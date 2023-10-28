c0141b3658a97bb209b4e9e757e256f4453e4e12
killbasa <killbasab@gmail.com>
2023-10-28 17:18:41 -0400

    chore: remove twitter meta tag

--- src/routes/(redirects)/invite/+page.svelte
+++ src/routes/(redirects)/invite/+page.svelte
@@ -3,6 +3,5 @@
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content="Petracord Invite" />
 	<meta property="og:description" content="Join the Petra Gurin fan Discord!" />
-	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="theme-color" content="#FFAE42" />
 </svelte:head>
